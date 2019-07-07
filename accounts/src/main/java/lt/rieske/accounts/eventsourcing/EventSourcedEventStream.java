971b006b73c4c010349621c471434d42fe417607
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2019-07-07 15:42:46 +0300

    Removed no longer needed conditional

--- accounts/src/main/java/lt/rieske/accounts/eventsourcing/EventSourcedEventStream.java
+++ accounts/src/main/java/lt/rieske/accounts/eventsourcing/EventSourcedEventStream.java
@@ -20,9 +20,6 @@ public class EventSourcedEventStream<T> implements EventStream<T> {
 
     @Override
     public void append(Event<T> event, T aggregate) {
-        if (event.aggregateId() != aggregateId) {
-            throw aggregateMismatch(event.aggregateId());
-        }
         long nextSequence = version + 1;
         eventStore.append(event, nextSequence);
         event.apply(aggregate);
@@ -45,10 +42,5 @@ public class EventSourcedEventStream<T> implements EventStream<T> {
         }
         events.forEach(event -> event.apply(aggregate));
         version += events.size();
-    }
-
-    private IllegalArgumentException aggregateMismatch(UUID eventAggregateId) {
-        return new IllegalArgumentException(String.format(
-                "Can not apply event, event.aggregateId()=%s, aggregateId=%s", eventAggregateId, aggregateId));
     }
 }
