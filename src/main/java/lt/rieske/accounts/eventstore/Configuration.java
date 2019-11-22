c151db1e1eb055a455e8ba1f759fbee9dfed85a3
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2019-11-22 13:59:26 +0200

    Fix mysql version, use json event serializer

--- src/main/java/lt/rieske/accounts/eventstore/Configuration.java
+++ src/main/java/lt/rieske/accounts/eventstore/Configuration.java
@@ -9,6 +9,6 @@ import javax.sql.DataSource;
 public class Configuration {
 
     public static EventStore<AccountEventsVisitor> accountEventStore(DataSource dataSource) {
-        return new SerializingEventStore<>(new MessagePackAccountEventSerializer(), new SqlEventStore(dataSource));
+        return new SerializingEventStore<>(new JsonEventSerializer<>(), new SqlEventStore(dataSource));
     }
 }
