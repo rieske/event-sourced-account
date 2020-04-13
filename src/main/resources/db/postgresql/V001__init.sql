9e360e5192f1c0c592c5b60d064f24afc6cddc02
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-04-13 20:55:56 +0300

    Remove Transaction table from postgres schema

--- src/main/resources/db/postgresql/V001__init.sql
+++ src/main/resources/db/postgresql/V001__init.sql
@@ -3,20 +3,14 @@ CREATE TABLE Event(
     sequenceNumber BIGINT NOT NULL,
     transactionId UUID NOT NULL,
     payload BYTEA NOT NULL,
-    PRIMARY KEY (aggregateId, sequenceNumber)
+    PRIMARY KEY(aggregateId, sequenceNumber)
 );
 
-CREATE INDEX idx_transaction ON Event (aggregateId, transactionId);
+CREATE INDEX idx_transaction ON Event(aggregateId, transactionId);
 
 CREATE TABLE Snapshot(
     aggregateId UUID NOT NULL,
     sequenceNumber BIGINT NOT NULL,
     payload BYTEA NOT NULL,
     PRIMARY KEY(aggregateId)
-);
-
-CREATE TABLE Transaction(
-    aggregateId UUID NOT NULL,
-    transactionId UUID NOT NULL,
-    PRIMARY KEY (aggregateId, transactionId)
 );
