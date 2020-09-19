354b4ad1abbaa065ee7c15e8d4bf384eeb12255d
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-09-19 16:37:13 +0300

    Extract test fixtures to separate source set

--- account-service/src/test/java/lt/rieske/accounts/ArchitectureTest.java
+++ account-service/src/test/java/lt/rieske/accounts/ArchitectureTest.java
@@ -1,14 +1,17 @@
 package lt.rieske.accounts;
 
 import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.core.importer.Location;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
+import java.util.regex.Pattern;
+
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 
-@AnalyzeClasses(packages = "lt.rieske.accounts", importOptions = ImportOption.DoNotIncludeTests.class)
+@AnalyzeClasses(packages = "lt.rieske.accounts", importOptions = ArchitectureTest.DoNotIncludeTests.class)
 class ArchitectureTest {
 
     @ArchTest
@@ -40,5 +43,18 @@ class ArchitectureTest {
     static final ArchRule eventstoreShouldNotDependOnApi =
             noClasses().that().resideInAPackage("lt.rieske.accounts.eventstore")
                     .should().dependOnClassesThat().resideInAPackage("lt.rieske.accounts.api");
+
+    static class DoNotIncludeTests implements ImportOption {
+        private static final Pattern TESTS_PATTERN = Pattern.compile(".*/build/classes/([^/]+/)?test/.*");
+        private static final Pattern TEST_FIXTURES_PATTERN = Pattern.compile(".*/build/libs/.*test-fixtures.jar!/.*");
+
+        @Override
+        public boolean includes(Location location) {
+            if (location.matches(TESTS_PATTERN)) {
+                return false;
+            }
+            return !location.matches(TEST_FIXTURES_PATTERN);
+        }
+    }
 }
 
