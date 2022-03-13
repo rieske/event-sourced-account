b23b017fd7bef8d1016f2b1b877bdfd025c63eea
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2022-03-13 11:50:03 +0200

    Add a line to check if coveralls catches this

--- account-service/src/main/java/lt/rieske/accounts/App.java
+++ account-service/src/main/java/lt/rieske/accounts/App.java
@@ -19,6 +19,7 @@ public class App {
                         getRequiredEnvVariable("DB_PASSWORD"), tracing, metrics),
                 System.getenv("ZIPKIN_URL")).start(8080);
         log.info("Server started on port: {}", port);
+        System.out.println("TESTING");
     }
 
     private static String getRequiredEnvVariable(String variableName) {
