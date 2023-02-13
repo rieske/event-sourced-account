1857edfc4b64178119f53e67a800f3db59739309
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2023-02-13 07:26:45 +0200

    Use UTF8MB4 encoding in mysql tables

--- eventstore/src/main/resources/db/migration/mysql/V001__init.sql
+++ eventstore/src/main/resources/db/migration/mysql/V001__init.sql
@@ -5,11 +5,11 @@ CREATE TABLE Event(
     payload BLOB NOT NULL,
     PRIMARY KEY (aggregateId, sequenceNumber),
     INDEX (aggregateId, transactionId)
-) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET=UTF8MB4;
 
 CREATE TABLE Snapshot(
     aggregateId BINARY(16) NOT NULL,
     sequenceNumber BIGINT NOT NULL,
     payload BLOB NOT NULL,
     PRIMARY KEY(aggregateId)
-) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET=UTF8MB4;
