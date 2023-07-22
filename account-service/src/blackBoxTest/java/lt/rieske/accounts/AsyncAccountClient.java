78b3ed33679d322bd84d056d0cc232082f2c4ae3
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2023-07-22 16:52:51 +0300

    Cleanup imports

--- account-service/src/blackBoxTest/java/lt/rieske/accounts/AsyncAccountClient.java
+++ account-service/src/blackBoxTest/java/lt/rieske/accounts/AsyncAccountClient.java
@@ -17,7 +17,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UncheckedIOException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
--- account-service/src/test/java/lt/rieske/accounts/eventstore/InMemoryEventStoreTests.java
+++ account-service/src/test/java/lt/rieske/accounts/eventstore/InMemoryEventStoreTests.java
@@ -17,7 +17,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
