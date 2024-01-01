cf5738836d657672903b9ae1629c8755cf51436c
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2024-01-01 15:16:33 +0200

    Add opentelemetry collector
    Route service logs and metrics via the otel collector.
    Refactor observability configuration.

--- account-service/src/main/java/lt/rieske/accounts/App.java
+++ account-service/src/main/java/lt/rieske/accounts/App.java
@@ -17,6 +17,7 @@ public class App {
                 (tracing, metrics) -> Configuration.blobEventStore(
                         getRequiredEnvVariable("JDBC_URL"), getRequiredEnvVariable("DB_USER"),
                         getRequiredEnvVariable("DB_PASSWORD"), tracing, metrics),
+                System.getenv("OPENTELEMETRY_URL"),
                 System.getenv("ZIPKIN_URL")).start(8080);
         log.info("Server started on port: {}", port);
     }
